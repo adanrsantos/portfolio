e4f46c874faa6df172b8857bfd1163cacd60654e
adanrsantos <adanrsantos2016@gmail.com>
2025-09-18 19:13:51 -0500

    deploy fix

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <div className='flex-1 flex flex-col justify-center items-center'>
         <h1 className='text-4xl font-bold'>About</h1>
-        <p>Hi my name is Adan and I'm from UTSA</p>
+        <p>{"Hi my name is Adan and I'm from UTSA"}</p>
     </div>
   )
 }
